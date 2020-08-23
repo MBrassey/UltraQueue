bc735fd30aea181df4f66f5559a842da39aa634f
luc1dLife <luc1d_life@protonmail.com>
2020-08-23 17:24:43 -0600

    no empty form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ var taskFormHandler = function (event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
